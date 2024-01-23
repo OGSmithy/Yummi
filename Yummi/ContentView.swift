677f1527b3538dd4cab1b9c48ac3444674203b99
OGSmithy <smith.o@etoncollege.org.uk>
2024-01-23 12:57:58 +0000

    Initial Commit

---
+++ Yummi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yummi
+//
+//  Created by Smith, Oli (AMM) on 23/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
