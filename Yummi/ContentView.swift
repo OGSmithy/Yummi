0f67967de8056831751c31a39dfc0c8f227cbf03
OGSmithy <smith.o@etoncollege.org.uk>
2024-01-30 13:14:34 +0000

    2

--- Yummi/ContentView.swift
+++ Yummi/ContentView.swift
@@ -9,17 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     @State var starterIngredients = StarterIngredients()
-    @State var selectedIngredient: Int = 0
-    
+    @State var selectedIngredient: Int = 0 {
+        didSet {
+            if selectedIngredient >= starterIngredients.ingredients.count {
+                selectedIngredient = 0
+            }
+        }
+    }
+    @State var newName: String = ""
+    @State var newQuantity: String = ""
+    @State var newCategory: String = ""
+    @State var newExpiryDate: String = ""
+    @State var newUnit: String = ""
+
     var body: some View {
-        VStack {
-            Text("\(starterIngredients.ingredients[selectedIngredient].displayIngredients())")
-            Button("Switch Ingredient", action: {
-                selectedIngredient += 1
-            })
+        Form {
+            VStack {
+                Text("\(starterIngredients.ingredients[selectedIngredient].displayIngredients())")
+                Button("Switch Ingredient", action: {
+                    selectedIngredient += 1
+                })
+                .padding()
+                
+                Text("Input a new ingredient: ")
+
+                TextField(
+                    "Name: ",
+                    text: $newName
+                )
+                
+                TextField(
+                    "Quantity: ",
+                    text: $newQuantity
+                )
+                
+                TextField(
+                    "Category: ",
+                    text: $newCategory
+                )
+                
+                TextField(
+                    "Expiry Date: ",
+                    text: $newExpiryDate
+                )
+                
+                TextField(
+                    "Unit: ",
+                    text: $newUnit
+                )
+            }
             
         }
-       
     }
 }
 
