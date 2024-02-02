cb66b7f989e1cca8f1c8e3d812ee27c8e1c5f3e8
OGSmithy <smith.o@etoncollege.org.uk>
2024-02-02 11:20:37 +0000

    Done 2

--- Yummi/ContentView.swift
+++ Yummi/ContentView.swift
@@ -8,75 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var starterIngredients = StarterIngredients()
+    @State var ingredients = [Ingredient(name: "Banana", quantity: 3.0, category: "Fruit", expiryDate: Date.distantPast, unit: "kg"),
+                              Ingredient(name: "Milk", quantity: 2.9, category: "Liquid", expiryDate: Date.now, unit: "L")]
+    
     @State var selectedIngredient: Int = 0 {
         didSet {
-            if selectedIngredient >= starterIngredients.ingredients.count {
+            if selectedIngredient >= ingredients.count {
                 selectedIngredient = 0
             }
         }
     }
-    @State var newName: String = String()
-    @State var newQuantity: String = String()
-    @State var newCategory: String = String()
-    @State var newExpiryDate: String = ""
-    @State var newUnit: String = String()
-    enum Category: String, CaseIterable, Identifiable {
+    
+    
+    @State private var newName: String = String()
+    @State private var newQuantity: String = String()
+    @State private var newCategory: Category = .fruit
+    @State private var newExpiryDate: Date = .now
+    @State private var newUnit: Unit = .kg
+   
+    enum Category: String, CaseIterable {
         case fruit, liquid, carbohydrates
-        var id: Self { self }
     }
-    
-    
-    @State private var myIngredients = [Ingredient]()
-    @State private var newIngredient = []
+    enum Unit: String, CaseIterable {
+        case kg, g, ml, L
+    }
+
     
     var body: some View {
         Form {
-            VStack {
-                Text("\(starterIngredients.ingredients[selectedIngredient].displayIngredients())")
-                Button("Switch Ingredient", action: {
-                    selectedIngredient += 1
-                })
-                .padding()
-                
+            
+            Section {
+                if ingredients.count > 0 {
+                    Text("\(ingredients[selectedIngredient].displayIngredients())")
+                    Button("Switch Ingredient", action: {
+                        selectedIngredient += 1
+                    })
+                    .padding()
+                }
+                else {
+                    Text("No ingredients. Add one")
+                }
+            }
+            
+            Section {
                 Text("Input a new ingredient: ")
-
+    
                 TextField(
                     "Name: ",
                     text: $newName
                 )
                 
-                
                 TextField(
                     "Quantity: ",
                     text: $newQuantity
                 )
-                newIngredient.append(newQuantity)
-                
+    
+    
                 Picker("Category", selection: $newCategory) {
-                    Text("Fruit").tag(Category.fruit)
-                    Text("Liquid").tag(Category.liquid)
-                    Text("Carbs").tag(Category.carbohydrates)
+                    ForEach(Category.allCases, id:\.self) { newCategory in
+                        Text(newCategory.rawValue.capitalized)
+                    }
                 }
                 
-                TextField(
-                    "Expiry Date: ",
-                    text: $newExpiryDate
-                )
-                newIngredient.append(newExpiryDate)
                 
-                TextField(
-                    "Unit: ",
-                    text: $newUnit
-                )
-                newIngredient.append(newUnit)
-                
-                Button("Create new ingredient", action: {
-                    myIngredients.append(newIngredient)
-                })
+                DatePicker("Expiry Date", selection: $newExpiryDate, displayedComponents: [.date])
+    
+                Picker("Unit", selection: $newUnit) {
+                    ForEach(Unit.allCases, id:\.self) { newUnit in
+                        Text(newUnit.rawValue)
+                    }
+                }
+    
+                Button {
+                    ingredients.append(Ingredient(name: newName, quantity: Double(newQuantity) ?? 0.0, category: newCategory.rawValue, expiryDate: newExpiryDate, unit: newUnit.rawValue))
+                } label: {
+                    Text("Create ingredient")
+                }
             }
             
         }
+        
     }
 }
 
--- Yummi/StarterIngredients.swift
+++ Yummi/StarterIngredients.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 struct StarterIngredients {
-    let ingredients = [Ingredient(name: "Banana", quantity: 3.0, category: "Fruit", expiryDate: Date.distantPast, unit: "kg"),
-                       Ingredient(name: "Milk", quantity: 2.9, category: "Liquid", expiryDate: Date.distantFuture, unit: "L")]
+    var ingredients = [Ingredient(name: "Banana", quantity: 3.0, category: "Fruit", expiryDate: Date.distantPast, unit: "kg"),
+                       Ingredient(name: "Milk", quantity: 2.9, category: "Liquid", expiryDate: Date.now, unit: "L")]
+    mutating func setNewIngredient(newIngredient: Ingredient) {
+        ingredients.append(newIngredient)
+    }
 }
